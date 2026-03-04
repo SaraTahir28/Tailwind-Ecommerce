41b59ff1342329e443a0edfdd316865376fc2609
Sara Tahir <saratahir34@gmail.com>
2026-03-04 16:35:26 +0000

    Tailwind css app with snipcart

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        serif:'DM Serif Text, serif',
+        sans:'DM Sans,sans-serif'
+      },
+      //
+    },
+  },
+  plugins: [],
+}
+
